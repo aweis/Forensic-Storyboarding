f480ac23bec30460c3c50c65ce8d30dbb0b268dd
Adam Weis <aweis@andrew.cmu.edu>
2012-04-15 14:48:19 -0400

    kesden's script

--- kesden.sh
+++ kesden.sh
@@ -9,7 +9,16 @@ read VIDEO_FILE
 
 if [ -f ${VIDEO_FILE} ]
 then
-  echo $VIDEO_FILE
+  echo "The file you want to analyze: ${VIDEO_FILE}"
+  #ffmpeg script from kesden
+  ffmpeg -g 600 -i ${VIDEO_FILE} tmpvideo.mpeg
+
+  #mplayer to grab the keyframes
+  mplayer -vo jpeg:outdir=frames -vf framestep=I -frames 100 tmpvideo.mpeg
+
+  #clean up tmpvideo.mpeg
+  rm tmpvideo.mpeg
+  
 else
   echo 'bad input'
 fi
